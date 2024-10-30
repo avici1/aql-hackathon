9ae15cbcbf0f8c19c230fb72728afb1b46980884
Avici1 <alainchristian87@gmail.com>
2024-10-30 11:50:30 +0000

    Added processing for station

--- Challenge_10/run.py
+++ Challenge_10/run.py
@@ -12,6 +12,7 @@ class Challenge_10:
         """
         Base constructor
         """
+        #Pylint C0103:invalid-name
         self.GOV_URL = "http://environment.data.gov.uk/flood-monitoring"
         self.CORE_URL = "https://api.core.aql.com/v1/"
         self.TOKEN = "12391|5wbWXrwLCxx5EatPBpxkLFksCTodQ1CxD3YZX8qp8ad8c808"
@@ -35,15 +36,36 @@ class Challenge_10:
             "L2403",
         ]
 
-    def process_station(self, station_id):
+    def process_station(self, s_id: str):
         """
         Processing the station
         """
-        url = f"{self.GOV_URL}/id/stations/{station_id}/measures"
+        url = f"{self.GOV_URL}/id/stations/{s_id}/measures"
         response = requests.get(url)
+        response.raise_for_status()
+        json_resp = response.json()
+        items = json_resp.get("items", [])
+
+        for item in items:
+            notation = item.get("notation")
+            l_reading = item.get("latestReading", {})
+
+            if not (notation and l_reading.get("value") and l_reading.get("dateTime")):
+                continue
+            utc_dt = dt.strptime(l_reading["dateTime"], "%Y-%m-%dT%H:%M:%SZ")
+            data = {
+                "reading": {
+                    "level": l_reading["value"],
+                    "timestamp": int(utc_dt.timestamp() * 1000),
+                }
+            }
+            print(data)
+            print(notation, end=": ")
+            print(l_reading["value"], end="")
+            print(item.get("unitName", ""))
 
 
 if __name__ == "__main__":
     ch_10 = Challenge_10()
     for station_id in ch_10.STATION_IDS:
-        print(station_id)
+        ch_10.process_station(station_id)
